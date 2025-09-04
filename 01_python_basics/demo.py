837d388caf4d417bae8b061d4a36b78dd3d78e1b
Pujabhartee <pujab@rgipt.ac.in>
2025-09-04 23:17:29 +0530

    Identifiers

--- 01_python_basics/demo.py
+++ 01_python_basics/demo.py
@@ -14,4 +14,30 @@ print('hello world')
 
 #import demo
 import keyword
-print(keyword.kwlist)
+print(keyword.kwlist)  
+
+
+# Identifiers
+
+data= "hello"
+# 2daydata= "hello" --> invalid as it starts with digit
+_2daydata = "hello"
+
+a=20
+A=30
+print(a)
+print(A)
+
+#class = "some data" -> keywords cannot be identifiers
+#print(class)
+
+# Define some student data
+# Bad way of using identifiers
+x="Ravi"
+y=25
+z="python course"
+
+# Good way of using identifiers
+student_firstname = "Puja"
+student_age = 29
+student_couse =  "python course"
