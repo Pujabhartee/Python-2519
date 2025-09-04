7a5b79b008e95bfcaa93a96a0956d4761b20cae5
Pujabhartee <pujab@rgipt.ac.in>
2025-09-04 16:21:23 +0530

    Some basic rules

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,17 @@
+# Comments - ignored by interpreter
+# a=10
+# b=20
+# print(a+b)
+
+#print(Hello World)
+print(9) # number - int
+print(9.5) # number - float
+greeting= "good morning" #text - string
+print(greeting)
+print('hello world')
+
+
+
+#import demo
+import keyword
+print(keyword.kwlist)
