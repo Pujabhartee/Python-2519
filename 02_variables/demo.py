6f2247a126d80c0a0bb8413f53a824bd4ed7c77d
Pujabhartee <pujab@rgipt.ac.in>
2025-09-11 23:57:31 +0530

    variables-interpolation

--- 02_variables/demo.py
+++ 02_variables/demo.py
@@ -64,3 +64,37 @@ b = "10"
 
 # print(x+a) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
 print(a+b)
+
+# Multiple Variables
+
+x,y,z = 10,20,30 # (LHS=RHS)
+# x,y,z = 10,20,30, 40  # ValueError: too many values to unpack (expected 3)
+print(x+y+z)
+
+s1,s2,s3 = "orange","apple","cherry"
+print(s1,s2+s3)
+
+# using concatenation 
+name = "Puja"
+age = 25
+# print("My name is " +name + "and I am" +age +"years old") # TypeError: can only concatenate str (not "int") to str
+print("My name is {name} and I am {age} years old")
+
+# using interpolation
+print(f"My name is {name} and I am {age} years old")
+
+# using different dynamic
+x,y,z = 10,20,30
+print(f"Sum of x,y,z,age: {x+y+z+age}")
+
+# school student info
+name = "john"
+# class = 8 # SyntaxError: invalid syntax (class is a keyword in python)
+school = "DPS"
+# print(f"My name is {name}, I am studying in {class} class in {school} school ")
+
+student_name = "john"
+student_class = 8
+student_school = "DPS"
+print(f"My name is {student_name}, I am studying in {student_class} class in {student_school} school ")
+
